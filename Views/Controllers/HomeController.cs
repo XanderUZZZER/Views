3b0f0e71f128e8a97448c7b579c753a4314069c0
XanderUZZZER <xanderuzzzer@gmail.com>
2019-03-05 17:33:27 +0200

    Add project files.

---
+++ Views/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Views.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            ViewBag.Message = "Hello, World";
+            ViewBag.Time = DateTime.Now.ToString("HH:mm:ss");
+            return View("DebugData");
+        }
+
+        public ViewResult List() => View();
+    }
+}
